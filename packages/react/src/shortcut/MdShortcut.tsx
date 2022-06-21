bc93bafdf6dce05ae5946f353fba0bae557795e4
erlendmiljo <erlend.vige@miljodir.no>
2022-06-21 11:01:01 +0200

    Add MdShortcut component

---
+++ packages/react/src/shortcut/MdShortcut.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ChevronIcon from '../icons/ChevronIcon';
+
+interface MdShortcutProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  to: string;
+}
+
+const MdShortcut = ({ to, children, ...otherProps }: MdShortcutProps) => {
+  return (
+    <a className="md-shortcut" {...otherProps} href={to}>
+      <div className="content">{children}</div>
+      <div className="icon">
+        <ChevronIcon color="#222" />
+      </div>
+    </a>
+  );
+};
+
+export default MdShortcut;
---
+++ stories/Shortcut.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import MdShortcut from '../packages/react/src/shortcut/MdShortcut';
+
+export default {
+  title: 'Components/Shortcut',
+  component: MdShortcut,
+};
+
+export const Primary = () => (
+  <MdShortcut
+    to="#"
+    onClick={(e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    }}
+  >
+    Dette er en snarvei
+  </MdShortcut>
+);
