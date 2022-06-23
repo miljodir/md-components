7fa0c7956966eec68e098323626f7b0f1bec6252
erlendmiljo <erlend.vige@miljodir.no>
2022-06-23 09:30:15 +0200

    Add MdShortcut + icons to react exports

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,8 @@
 import MdButton from './button/MdButton';
 import MdCheckbox from './checkbox/MdCheckbox';
 import MdLink from './link/MdLink';
+import MdShortcut from './shortcut/MdShortcut';
+import ChevronIcon from './icons/ChevronIcon';
+import ConfirmIcon from './icons/ConfirmIcon';
 
-export { MdButton, MdCheckbox, MdLink };
+export { MdButton, MdCheckbox, MdLink, MdShortcut, ChevronIcon, ConfirmIcon };
