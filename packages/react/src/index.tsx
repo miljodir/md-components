76a3c1199254dc309f45594d07bda2a6b1372871
erlendmiljo <erlend.vige@miljodir.no>
2022-06-28 11:42:57 +0200

    Export DataGrid modules from react package

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -5,4 +5,26 @@ import MdShortcut from './shortcut/MdShortcut';
 import ChevronIcon from './icons/ChevronIcon';
 import ConfirmIcon from './icons/ConfirmIcon';
 
-export { MdButton, MdCheckbox, MdLink, MdShortcut, ChevronIcon, ConfirmIcon };
+import MdDataGrid from './data-grid/MdDataGrid';
+import MdDataColumnHeader from './data-grid/MdDataGridColumnHeader';
+import MdDataColumnHeaders from './data-grid/MdDataGridColumnHeaders';
+import MdDataColumnRows from './data-grid/MdDataGridRows';
+import MdDataColumnRow from './data-grid/MdDataGridRow';
+import MdDataColumnRowValue from './data-grid/MdDataGridRowValue';
+import MdDataColumnRightAlignedContent from './data-grid/MdDataGridRightAlignedContent';
+
+export {
+  MdButton,
+  MdCheckbox,
+  MdLink,
+  MdShortcut,
+  ChevronIcon,
+  ConfirmIcon,
+  MdDataGrid,
+  MdDataColumnHeader,
+  MdDataColumnHeaders,
+  MdDataColumnRows,
+  MdDataColumnRow,
+  MdDataColumnRowValue,
+  MdDataColumnRightAlignedContent,
+};
