bb21dc7a80bf1173cc1c1aecddac7f63a0ddbe7d
Jonathan Aanesen <jonathan.aanesen@gmail.com>
2023-04-17 09:29:29 +0200

    feat: boolean to disable outside click

--- packages/react/src/modal/MdModal.tsx
+++ packages/react/src/modal/MdModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import classnames from 'classnames';
-import { v4 as uuidv4 } from 'uuid';
 
 import MdClickOutsideWrapper from '../utils/MdClickOutsideWrapper';
 import MdXIcon from '../icons/MdXIcon';
@@ -13,8 +12,9 @@ interface MdModalProps {
   open?: boolean;
   error?: boolean;
   className?: string;
+  closeOnOutsideClick?: boolean;
   onClose?(e: React.MouseEvent): void;
-};
+}
 
 const MdModal: React.FunctionComponent<MdModalProps> = ({
   children,
@@ -24,19 +24,23 @@ const MdModal: React.FunctionComponent<MdModalProps> = ({
   open = false,
   error = false,
   className = '',
-  onClose
+  closeOnOutsideClick = true,
+  onClose,
 }: MdModalProps) => {
-
-  const classNames = classnames('md-modal', {
-    'md-modal--open': !!open,
-    'md-modal--error': !!error,
-  }, className);
+  const classNames = classnames(
+    'md-modal',
+    {
+      'md-modal--open': !!open,
+      'md-modal--error': !!error,
+    },
+    className
+  );
 
   const closeModal = (e: React.MouseEvent) => {
     if (onClose) {
       onClose(e);
     }
-  }
+  };
 
   if (!open) {
     return null;
@@ -44,30 +48,30 @@ const MdModal: React.FunctionComponent<MdModalProps> = ({
 
   return (
     <>
-      <div className="md-modal__overlay"></div>
+      <div className="md-modal__overlay" />
       <div className={classNames}>
-        <div
-          className="md-modal__content"
-        >
+        <div className="md-modal__content">
           <MdClickOutsideWrapper
-            onClickOutside={(e) => { closeModal(e) }}
+            onClickOutside={(e) => {
+              if (closeOnOutsideClick) closeModal(e);
+            }}
             className="md-modal__inner-wrapper"
           >
             <div className="md-modal__header">
-              <div className='md-modal__header-content'>
+              <div className="md-modal__header-content">
                 {headingIcon}
                 {heading}
               </div>
               <button
                 className="md-modal__close-button"
-                onClick={(e) => { closeModal(e) }}
+                onClick={(e) => {
+                  closeModal(e);
+                }}
               >
                 <MdXIcon className="md-modal__close-button-icon" />
               </button>
             </div>
-            <div className="md-modal__content-inner">
-              { children }
-            </div>
+            <div className="md-modal__content-inner">{children}</div>
           </MdClickOutsideWrapper>
         </div>
       </div>
