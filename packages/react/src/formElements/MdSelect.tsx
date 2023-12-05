7f46948b7953556ec850d59b990c8d020fe1e9d9
Kajsa Eggum <kajsa.eggum@geodata.no>
2023-12-05 15:25:59 +0100

    make input and select ref props actual forward refs that work...

--- packages/react/src/formElements/MdSelect.tsx
+++ packages/react/src/formElements/MdSelect.tsx
@@ -11,172 +11,193 @@ import MdClickOutsideWrapper from '../utils/MdClickOutsideWrapper';
 interface MdSelectOptionProps {
   text: string;
   value: string;
-};
+}
 
 export interface MdSelectProps {
-    label?: string | null;
-    options?: MdSelectOptionProps[];
-    id?: string | number | null | undefined;
-    onChange(e: MdSelectOptionProps): void;
-    name?: string;
-    value?: string | number;
-    placeholder?: string;
-    disabled?: boolean;
-    size?: string;
-    helpText?: string;
-    error?: boolean;
-    errorText?: string;
-    selectRef?: React.Ref<HTMLButtonElement>;
-};
+  label?: string | null;
+  options?: MdSelectOptionProps[];
+  id?: string | number | null | undefined;
+  onChange(e: MdSelectOptionProps): void;
+  name?: string;
+  value?: string | number;
+  placeholder?: string;
+  disabled?: boolean;
+  size?: string;
+  helpText?: string;
+  error?: boolean;
+  errorText?: string;
+}
 
-const MdSelect: React.FunctionComponent<MdSelectProps> = ({
-  label,
-  value,
-  options,
-  id = null,
-  name,
-  placeholder = 'Vennligst velg',
-  disabled = false,
-  size,
-  helpText,
-  error = false,
-  errorText,
-  onChange,
-  selectRef
-}: MdSelectProps) => {
-  const [open, setOpen] = useState(false);
-  const [helpOpen, setHelpOpen] = useState(false);
+const MdSelect = React.forwardRef<HTMLButtonElement, MdSelectProps>(
+  (
+    {
+      label,
+      value,
+      options,
+      id = null,
+      name,
+      placeholder = 'Vennligst velg',
+      disabled = false,
+      size,
+      helpText,
+      error = false,
+      errorText,
+      onChange,
+    },
+    ref
+  ) => {
+    const [open, setOpen] = useState(false);
+    const [helpOpen, setHelpOpen] = useState(false);
 
-  const uuid = id || uuidv4();
+    const uuid = id || uuidv4();
 
-  const classNames = classnames('md-select', {
-    'md-select--open': !!open,
-    'md-select--error': !!error,
-    'md-select--disabled': !!disabled,
-    'md-select--medium': size === 'medium',
-    'md-select--small': size === 'small'
-  });
+    const classNames = classnames('md-select', {
+      'md-select--open': !!open,
+      'md-select--error': !!error,
+      'md-select--disabled': !!disabled,
+      'md-select--medium': size === 'medium',
+      'md-select--small': size === 'small',
+    });
 
-  const selectedOption = value && value !== '' ? options && options.find(o => o.value === value) : '';
+    const selectedOption =
+      value && value !== ''
+        ? options && options.find((o) => o.value === value)
+        : '';
 
-  let displayValue = placeholder;
-  if (selectedOption && selectedOption.value) {
-    displayValue = selectedOption.text;
-  }
-  if (open) {
-    displayValue = '';
-  }
-
-  useEffect(() => {
-    document.addEventListener("keydown", onKeyDown, false);
-
-    return () => {
-      document.removeEventListener("keydown", onKeyDown, false);
+    let displayValue = placeholder;
+    if (selectedOption && selectedOption.value) {
+      displayValue = selectedOption.text;
     }
-  });
-
-  const onKeyDown = (e: any) => {
     if (open) {
-      const reg = /[a-z\Wæøå]+/igm;
-      const key = e.key;
-      if(key && reg.test(key) && key.length === 1) {
-        const option = options?.find(o => (o.text?.startsWith(key.toLowerCase()) || o.text?.startsWith(key.toUpperCase())));
-        if (option) {
-          /* Find corresponding button */
-          const button = document.getElementById(`md-select-option-${uuid}-${option.value}`);
-          if (button) {
-            button.focus();
+      displayValue = '';
+    }
+
+    useEffect(() => {
+      document.addEventListener('keydown', onKeyDown, false);
+
+      return () => {
+        document.removeEventListener('keydown', onKeyDown, false);
+      };
+    });
+
+    const onKeyDown = (e: any) => {
+      if (open) {
+        const reg = /[a-z\Wæøå]+/gim;
+        const key = e.key;
+        if (key && reg.test(key) && key.length === 1) {
+          const option = options?.find(
+            (o) =>
+              o.text?.startsWith(key.toLowerCase()) ||
+              o.text?.startsWith(key.toUpperCase())
+          );
+          if (option) {
+            /* Find corresponding button */
+            const button = document.getElementById(
+              `md-select-option-${uuid}-${option.value}`
+            );
+            if (button) {
+              button.focus();
+            }
           }
         }
       }
-    }
-  }
+    };
 
-  const handleOptionClick = (option: MdSelectOptionProps) => {
-    onChange(option);
-    setOpen(false);
-  }
+    const handleOptionClick = (option: MdSelectOptionProps) => {
+      onChange(option);
+      setOpen(false);
+    };
 
-  const isSelectedOption = (option: MdSelectOptionProps) => {
-    return value && value !== '' && value == option.value;
-  }
+    const isSelectedOption = (option: MdSelectOptionProps) => {
+      return value && value !== '' && value == option.value;
+    };
 
-  const buttonClasseNames = classnames('md-select__button', {
-    'md-select__button--open': !!open,
-    'md-select__button--error': !!error
-  });
-
-  const optionClass = (option: MdSelectOptionProps) => {
-    return classnames('md-select__dropdown-item', {
-      'md-select__dropdown-item--selected': isSelectedOption(option)
+    const buttonClasseNames = classnames('md-select__button', {
+      'md-select__button--open': !!open,
+      'md-select__button--error': !!error,
     });
-  }
 
-  return (
-    <div className={classNames}>
-      <div className="md-select__label">
-        <div>{label}</div>
-        {helpText && helpText !== '' &&
-          <div className="md-select__help-button">
-            <MdHelpButton
-              onClick={() => setHelpOpen(!helpOpen)}
-              expanded={helpOpen}
-            />
-          </div>
-        }
-      </div>
+    const optionClass = (option: MdSelectOptionProps) => {
+      return classnames('md-select__dropdown-item', {
+        'md-select__dropdown-item--selected': isSelectedOption(option),
+      });
+    };
 
-      {helpText && helpText !== '' &&
-        <div className={`md-select__help-text ${helpOpen ? 'md-select__help-text--open' : ''}`}>
-          <MdHelpText>{ helpText }</MdHelpText>
+    return (
+      <div className={classNames}>
+        <div className="md-select__label">
+          <div>{label}</div>
+          {helpText && helpText !== '' && (
+            <div className="md-select__help-button">
+              <MdHelpButton
+                onClick={() => setHelpOpen(!helpOpen)}
+                expanded={helpOpen}
+              />
+            </div>
+          )}
         </div>
-      }
 
-      <MdClickOutsideWrapper
-        onClickOutside={() => setOpen(false)}
-        className='md-select__container'
-      >
-        <button
-          className={buttonClasseNames}
-          type='button'
-          tabIndex={0}
-          onClick={() => !disabled && setOpen(!open)}
-          ref={selectRef}
+        {helpText && helpText !== '' && (
+          <div
+            className={`md-select__help-text ${
+              helpOpen ? 'md-select__help-text--open' : ''
+            }`}
+          >
+            <MdHelpText>{helpText}</MdHelpText>
+          </div>
+        )}
+
+        <MdClickOutsideWrapper
+          onClickOutside={() => setOpen(false)}
+          className="md-select__container"
         >
-          <div className="md-select__button-text">{displayValue}</div>
-          <div className="md-select__button-icon">
-            <MdChevronIcon />
-          </div>
-        </button>
+          <button
+            className={buttonClasseNames}
+            type="button"
+            tabIndex={0}
+            onClick={() => !disabled && setOpen(!open)}
+            ref={ref}
+          >
+            <div className="md-select__button-text">{displayValue}</div>
+            <div className="md-select__button-icon">
+              <MdChevronIcon />
+            </div>
+          </button>
 
-        {options && options.length > 0 &&
-          <div className="md-select__dropdown">
-            {options.map(option => (
-              <button
-                key={`md-select-option-${uuid}-${option.value}`}
-                id={`md-select-option-${uuid}-${option.value}`}
-                type='button'
-                tabIndex={open ? 0: -1}
-                className={optionClass(option)}
-                onClick={() => open && handleOptionClick(option)}
-              >
-                <div className="md-select__dropdown-item-text">{option.text}</div>
-                {isSelectedOption(option) &&
-                  <div className="md-select__dropdown-item-clear" title="Klikk for å fjerne valg">
-                    <MdXIcon />
+          {options && options.length > 0 && (
+            <div className="md-select__dropdown">
+              {options.map((option) => (
+                <button
+                  key={`md-select-option-${uuid}-${option.value}`}
+                  id={`md-select-option-${uuid}-${option.value}`}
+                  type="button"
+                  tabIndex={open ? 0 : -1}
+                  className={optionClass(option)}
+                  onClick={() => open && handleOptionClick(option)}
+                >
+                  <div className="md-select__dropdown-item-text">
+                    {option.text}
                   </div>
-                }
-              </button>
-            ))}
-          </div>
-        }
-      </MdClickOutsideWrapper>
+                  {isSelectedOption(option) && (
+                    <div
+                      className="md-select__dropdown-item-clear"
+                      title="Klikk for å fjerne valg"
+                    >
+                      <MdXIcon />
+                    </div>
+                  )}
+                </button>
+              ))}
+            </div>
+          )}
+        </MdClickOutsideWrapper>
 
-      {error && errorText && errorText !== '' &&
-        <div className="md-select__error">{errorText}</div>
-      }
-    </div>
-  );
-};
+        {error && errorText && errorText !== '' && (
+          <div className="md-select__error">{errorText}</div>
+        )}
+      </div>
+    );
+  }
+);
 
 export default MdSelect;
