2d1eb3d642d9c5b2c31aeedb5cad0350a97d7b4b
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-05-26 21:26:27 +0200

    fix: Install lodash to fix  issue with deploying to npm

--- packages/react/src/tooltip/MdTooltip.tsx
+++ packages/react/src/tooltip/MdTooltip.tsx
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import { debounce } from 'lodash';
+import debounce from 'lodash.debounce';
 import React, { useState, useCallback } from 'react';
 
 export interface MdTooltipProps {
