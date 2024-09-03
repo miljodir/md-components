aeb8b4379fd05a60c5175a54c4385eb69e05c973
Aurora Scharff <66901228+aurorascharff@users.noreply.github.com>
2024-09-03 12:07:00 +0200

    Correctly extend MdLink and MdTile with button/achor-attributes (#159)

--- packages/react/src/link/MdLink.tsx
+++ packages/react/src/link/MdLink.tsx
@@ -1,33 +1,33 @@
 import classnames from 'classnames';
 import React, { forwardRef } from 'react';
 
-export interface MdLinkProps {
+export type MdLinkProps = {
   children?: string | React.ReactNode;
   href?: string;
   onClick?(_e: React.MouseEvent): void;
-}
+} & React.AnchorHTMLAttributes<HTMLAnchorElement> &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const MdLink = forwardRef<
-  HTMLButtonElement | HTMLAnchorElement,
-  MdLinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement> & React.ButtonHTMLAttributes<HTMLButtonElement>
->(({ href, children, onClick, ...otherProps }, ref) => {
-  const classNames = classnames('md-link', otherProps.className);
-  return href ? (
-    <a href={href} {...otherProps} className={classNames} ref={ref as React.ForwardedRef<HTMLAnchorElement>}>
-      {children}
-    </a>
-  ) : (
-    <button
-      type="button"
-      onClick={onClick}
-      {...otherProps}
-      className={classNames}
-      ref={ref as React.ForwardedRef<HTMLButtonElement>}
-    >
-      {children}
-    </button>
-  );
-});
+const MdLink = forwardRef<HTMLAnchorElement | HTMLButtonElement, MdLinkProps>(
+  ({ href, children, onClick, ...otherProps }, ref) => {
+    const classNames = classnames('md-link', otherProps.className);
+    return href ? (
+      <a href={href} {...otherProps} className={classNames} ref={ref as React.ForwardedRef<HTMLAnchorElement>}>
+        {children}
+      </a>
+    ) : (
+      <button
+        type="button"
+        onClick={onClick}
+        {...otherProps}
+        className={classNames}
+        ref={ref as React.ForwardedRef<HTMLButtonElement>}
+      >
+        {children}
+      </button>
+    );
+  },
+);
 
 MdLink.displayName = 'MdLink';
 export default MdLink;
