698ce16b2cf47f453d8c3ecd9f970a9e06cb9150
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-14 09:54:30 +0100

    Update stepper storybook example and extend div props on MdStep

--- packages/react/src/stepper/MdStep.tsx
+++ packages/react/src/stepper/MdStep.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export interface MdStepProps {
+export interface MdStepProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
   children: React.ReactNode;
   completedContent?: React.ReactNode;
 }
 
-const MdStep: React.FunctionComponent<MdStepProps> = ({ children }: MdStepProps) => {
-  return <div>{children}</div>;
+const MdStep: React.FunctionComponent<MdStepProps> = ({ children, ...otherProps }: MdStepProps) => {
+  return <div {...otherProps}>{children}</div>;
 };
 
 export default MdStep;
