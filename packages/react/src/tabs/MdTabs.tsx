fc76aad03619697ffebdf53dcc16d0e7b8a34f0a
Ola Helland-Pedersen <52152379+ohp-inmeta@users.noreply.github.com>
2025-06-24 09:09:41 +0200

    Rewrite MdTabs to Ariakit (#327)
    Remove unused state / select handling (ariakit handles this now)

--- packages/react/src/tabs/MdTabs.tsx
+++ packages/react/src/tabs/MdTabs.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import React, { useState } from 'react';
+import * as Ariakit from '@ariakit/react';
+import React from 'react';
 import MdTabTitle from './MdTabTitle';
+import type { MdTabProps } from './MdTab';
 import type { ReactElement, ReactNode } from 'react';
 
 export interface MdTabsProps {
@@ -19,29 +21,36 @@ export const MdTabs: React.FunctionComponent<MdTabsProps> = ({
   chipsPrefixIcon = false,
   compact = false,
 }: MdTabsProps) => {
-  const [selectedTab, setSelectedTab] = useState(initialTab);
-
-  const tabs = children instanceof Array ? children : [children];
+  const tabs: ReactElement<MdTabProps>[] = children instanceof Array ? children : [children];
+  const selectedTab = initialTab > tabs.length - 1 || initialTab < 0 ? 0 : initialTab;
 
   return (
     <div className={`md-tabs-container${compact ? ' md-tabs__compact' : ''}`}>
-      <ul>
-        {tabs.map((item, index) => {
-          return (
-            <MdTabTitle
-              key={`md-tab-${index}`}
-              title={item.props.title}
-              index={index}
-              disabled={item.props.disabled}
-              selectedTab={selectedTab}
-              setSelectedTab={setSelectedTab}
-              chips={chips}
-              chipsPrefixIcon={chipsPrefixIcon}
-            />
-          );
-        })}
-      </ul>
-      {tabs[selectedTab]}
+      <Ariakit.TabProvider defaultSelectedId={`md-tab-${selectedTab}`} selectOnMove={false}>
+        <Ariakit.TabList className="md-tabs-list">
+          {tabs.map((item, index) => {
+            return (
+              <MdTabTitle
+                key={`md-tab-${index}`}
+                title={item.props.title}
+                index={index}
+                disabled={item.props.disabled}
+                chips={chips}
+                chipsPrefixIcon={chipsPrefixIcon}
+              />
+            );
+          })}
+        </Ariakit.TabList>
+        <div className="md-tabs-panels">
+          {tabs.map((item, index) => {
+            return (
+              <Ariakit.TabPanel key={`md-tab-panel-${index}`} tabId={`md-tab-${index}`} className="md-tab-panel">
+                {item.props.children}
+              </Ariakit.TabPanel>
+            );
+          })}
+        </div>
+      </Ariakit.TabProvider>
     </div>
   );
 };
