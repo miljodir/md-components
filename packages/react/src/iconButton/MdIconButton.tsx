300399ea97e1c83c851771f3faf61b7910c0ec34
Helge Johnsen <helge.johnsen@crayon.no>
2025-11-27 11:49:39 +0100

    Replaces aria-label with labels prop in MdIconButton
    Deprecates the `aria-label` prop in `MdIconButton` and introduces a `labels` prop to provide separate labels for the button and tooltip.
    
    This change provides more flexibility and clarity in defining labels for accessibility, addressing potential ambiguity when the same label is used for both the button and its tooltip.
    A warning message is displayed if the deprecated `aria-label` is used to encourage migration to the new `labels` prop.

--- packages/react/src/iconButton/MdIconButton.tsx
+++ packages/react/src/iconButton/MdIconButton.tsx
@@ -5,12 +5,18 @@ import React from 'react';
 import MdLoadingSpinner from '../loadingSpinner/MdLoadingSpinner';
 import MdTooltip from '../tooltip/MdTooltip';
 
+interface Labels {
+  button?: string;
+  toolTip?: string;
+}
+
 export interface MdIconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  labels?: Labels;
   theme?: 'filled' | 'border' | 'plain';
   children?: React.ReactNode;
   disabled?: boolean;
   showTooltip?: boolean;
-  ['aria-label']: string;
+  ['aria-label']?: string;
   loading?: boolean;
 }
 
@@ -21,6 +27,7 @@ export const MdIconButton: React.FunctionComponent<MdIconButtonProps> = ({
   disabled,
   type = 'button',
   className,
+  labels = {},
   'aria-label': ariaLabel,
   loading = false,
   ...otherProps
@@ -34,8 +41,19 @@ export const MdIconButton: React.FunctionComponent<MdIconButtonProps> = ({
     className,
   );
 
+  if (ariaLabel == null || ariaLabel.length !== 0) {
+    console.warn('MdIconButton: The prop "aria-label" is deprecated and replaces by labels. Please use labels.button and labels.toolTip instead.');
+  }
+
+  const defaultLabels: Required<Labels> = {
+    button: ariaLabel || '',
+    toolTip: ariaLabel || '',
+  };
+
+  const mergedLabels: Required<Labels> = { ...defaultLabels, ...labels };
+
   const button = (
-    <button aria-label={ariaLabel} type={type} disabled={disabled} className={classNames} {...otherProps}>
+    <button aria-label={mergedLabels.button} type={type} disabled={disabled} className={classNames} {...otherProps}>
       {children && (
         <div aria-hidden="true" className="md-icon-button__icon">
           {loading ? <MdLoadingSpinner /> : children}
@@ -45,7 +63,7 @@ export const MdIconButton: React.FunctionComponent<MdIconButtonProps> = ({
   );
 
   return showTooltip && !disabled ? (
-    <MdTooltip unmountOnHide tooltipContent={ariaLabel} aria-label={ariaLabel}>
+    <MdTooltip unmountOnHide tooltipContent={mergedLabels.toolTip} aria-label={mergedLabels.toolTip}>
       {button}
     </MdTooltip>
   ) : (
--- stories/IconButton.stories.tsx
+++ stories/IconButton.stories.tsx
@@ -58,8 +58,20 @@ export default {
         },
       },
     },
+    labels: {
+      type: { name: 'object' },
+      description:
+        'Object containing custom labels for button aria-labels. Properties: `button`, `toolTip`. Falls back to empty string.',
+      table: {
+        defaultValue: { summary: '{ button: "", toolTip: ""}' },
+        type: {
+          summary: '{ button?: string; toolTip?: string; }',
+        },
+      },
+      control: { type: 'object' },
+    },     
     'aria-label': {
-      description: 'The aria label for the tooltip',
+      description: 'The aria label for the tooltip. NOTE! This prop is deprecated and replaced by labels.',
       table: {
         type: {
           summary: 'string',
@@ -115,6 +127,7 @@ const Template = (args: MdIconButtonProps) => {
 export const Filled = Template.bind({});
 Filled.args = {
   theme: 'filled',
+  labels: { button: 'Last ned filen', toolTip: 'Last ned filen' },
   'aria-label': 'Last ned filen',
   showTooltip: true,
   disabled: false,
@@ -124,6 +137,7 @@ Filled.args = {
 export const Border = Template.bind({});
 Border.args = {
   theme: 'border',
+  labels: { button: 'Last ned filen', toolTip: 'Last ned filen' },
   'aria-label': 'Last ned filen',
   showTooltip: true,
   disabled: false,
@@ -133,6 +147,7 @@ Border.args = {
 export const Plain = Template.bind({});
 Plain.args = {
   theme: 'plain',
+  labels: { button: 'Last ned filen', toolTip: 'Last ned filen' },
   'aria-label': 'Last ned filen',
   showTooltip: true,
   disabled: false,
