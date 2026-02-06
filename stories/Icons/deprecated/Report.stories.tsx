6eefe0999b1568da1c034f7b966825043e5b4a7b
Helge Johnsen <helge.johnsen@crayon.no>
2026-02-06 10:04:54 +0100

    Deprecates MdIconReport
    Deprecates the MdIconReport component and replaces it with MdIconDangerous.
    
    Adds a console warning to inform users about the deprecation and suggest the replacement.
    Updates the story to reflect the deprecation.

--- stories/Icons/deprecated/Report.stories.tsx
+++ stories/Icons/deprecated/Report.stories.tsx
@@ -13,7 +13,7 @@ export default {
       description: {
         component:
           // eslint-disable-next-line quotes
-          "Report icon. Color can be inherited from parent, or set directly on the component. In addition to the properties presented here, the component accepts all standard attributes of a HTML SVG element.<br/><br/>`import { MdReportIcon } from '@miljodirektoratet/md-react'`",
+          "Deprecated. Replaced by Dangerous. Color can be inherited from parent, or set directly on the component. In addition to the properties presented here, the component accepts all standard attributes of a HTML SVG element.<br/><br/>`import { MdReportIcon } from '@miljodirektoratet/md-react'`",
       },
     },
   },
